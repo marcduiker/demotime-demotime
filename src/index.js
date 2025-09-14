2223867e1c8c928df02fbc1ae5840a104cb6f016
Marc Duiker <marcduiker@users.noreply.github.com>
2025-09-14 21:59:53 +0000

    Update code examples
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+
 for (let i = 0; i < 10; i++) {
   //<!-- START -->
   //<!-- END -->
-  await sleep(500);
 }
 
 console.log("Demo app is finished.");
